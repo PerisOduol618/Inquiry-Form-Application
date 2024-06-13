34c216b2973aa90da11fd2f021bf3b1664554974
Oduol Peris <oduolpepe618@gmail.com>
2024-06-13 20:57:12 +0300

    import  BrowserModule,ReactiveFormsModule,HttpClientModule

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { InquiryFormComponent } from './components/inquiry-form/inquiry-form.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    InquiryFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
